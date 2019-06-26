2a729ac06ffb5449889311df2e9adeec7c75b6b3
Fabricio Masiero <masiero572@gmail.com>
2019-06-25 22:33:47 -0300

    Removin Swift Package Dependencies

--- Politize/Domain/Features/Feed/Models/Document/Feed/Feed Fields/ImageUrl/ImageUrl.swift
+++ Politize/Domain/Features/Feed/Models/Document/Feed/Feed Fields/ImageUrl/ImageUrl.swift
@@ -18,11 +18,4 @@ public struct ImageUrl: Codable, Hashable, Identifiable {
         case id
         case value = "stringValue"
     }
-    
-    public func imageUrl() -> URL {
-        guard let url = URL(string: value) else {
-            return URL(string: "about:blank")!
-        }
-        return URL(string: "https://www.politize.com.br/wp-content/uploads/2019/04/como-cientistas-fazem-politica-politize-destaque-495x400.jpg")!
-    }
 }
